b0995b8df1ab179b4d5eaa48437d8350bf1bc3f0
JuliaZaykaS <sys55@mail.ru>
2021-06-05 16:24:50 +0300

    фильтр стран

---
+++ src/index.js
@@ -0,0 +1,77 @@
+import css from "./css/styles.css";
+import normalize from "./css/modern-normalize.css";
+import modal from "./css/modal.css";
+import refs from "./js/refs.js";
+import arr from "./db/countries.json";
+import searchItemTemplate from "./template/countrySearchItem.hbs";
+import modalItemTemplate from "./template/modalCountryItem.hbs";
+
+// console.log('countries', arr);
+
+
+const { formEl, searchResultEl, countriesListEl, modalEl, modalContentEl } = refs;
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // console.dir(e.target);
+    // console.log(e.target.elements.search.value);
+    let search = e.target.elements.search.value.toLowerCase();
+    let country = arr.filter(elem => elem.name.toLowerCase().includes(search));
+    console.log(search);
+    console.log(country);
+    let items = searchItemTemplate(country);
+    console.log(items);
+    searchResultEl.insertAdjacentHTML('afterbegin', items);
+
+    // Теперь вешаем слушатель событий по клику на каждый найденный элемент
+
+    console.log([...searchResultEl.children]);
+    [...searchResultEl.children].forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            // console.log(e.target.textContent.trim());
+            let search = e.target.textContent.trim();
+            let country = arr.filter(elem => elem.name === search);
+            console.log(country);
+            let items = modalItemTemplate(country);
+            console.log(items);
+            modalContentEl.insertAdjacentHTML('afterbegin', items);
+            modalEl.classList.remove('is-hidden');
+        })
+    })
+
+
+});
+
+window.addEventListener('keydown', (e) => {
+    console.log(e.code);
+    if (e.code === 'Escape') {
+        modalEl.classList.add('is-hidden');
+        modalContentEl.innerHTML = '';
+    }
+});
+
+modalEl.addEventListener('click', (e) => {
+    if (e.target !== e.currentTarget) return;
+    // if (e.target.id === 'modal') {
+
+    modalEl.classList.add('is-hidden');
+    modalContentEl.innerHTML = '';
+    // }
+
+
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    // должны отрисоваться все странцы по шаблону countrieslist,
+    // передать массив arr, сохранить в переменную items,
+    // полученную разметку вставить в countriesListEl,
+    // на каждый элемент повесить слушатель событий, получить данные
+    // и при открытии модалки отрисовывать
+    // по textContent элемента сделать фильтр массива,
+    // отфильстрованный массив передавать в modalItemTemplate для отрисовку
+})
+
+
+
+
+
---
+++ src/js/refs.js
@@ -0,0 +1,9 @@
+
+export default {
+    formEl: document.getElementById("search-form"),
+    searchResultEl: document.getElementById('search-results'),
+    countriesListEl: document.getElementById('coutries-list'),
+    modalEl: document.getElementById('modal'),
+    modalContentEl: document.getElementById('modal-content')
+
+};
